f16f44b48ba3a38e2ffd8086d8ae59a9b04dfa1a
Robin Bellier <robin.bellier@etu.univ-grenoble-alpes.fr>
2024-11-26 13:00:44 +0100

    début du css

---
+++ src/Controller/EleveController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EleveController extends AbstractController
+{
+    #[Route('/eleve', name: 'app_eleve')]
+    public function index(): Response
+    {
+        return $this->render('eleve/index.html.twig', [
+            'controller_name' => 'EleveController',
+        ]);
+    }
+}
--- src/Repository/EleveRepository.php
+++ src/Repository/EleveRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Classe;
 use App\Entity\Eleve;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,6 +20,23 @@ class EleveRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Eleve::class);
+    }
+
+    public function search(string $name = null, Classe $classe = null)
+    {
+        $requete = $this->createQueryBuilder('u');
+
+        if ($name) {
+            $requete->andWhere('u.nom LIKE :search or u.prenom LIKE :search')
+                ->setParameter('search', '%' . $name . '%');
+        }
+
+        if ($classe) {
+            $requete->andWhere('u.classe = :classe')
+                ->setParameter('classe', $classe->getId());
+        }
+
+        return $requete->getQuery()->getResult();
     }
 
 //    /**

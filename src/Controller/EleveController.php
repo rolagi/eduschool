36375a84f568b8393cb777561c564625db5ce5ba
Robin Bellier <robin.bellier@etu.univ-grenoble-alpes.fr>
2024-11-26 17:08:55 +0100

    avancée sur les controleurs

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ClasseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EleveController extends AbstractController
 {
-    #[Route('/eleve', name: 'app_eleve')]
-    public function index(): Response
+    #[Route('/eleves', name: 'app_eleve')]
+    public function index(ClasseRepository $classeRepository): Response
     {
+
+        $classes = $classeRepository->findAll();
+dd($classes);
         return $this->render('eleve/index.html.twig', [
             'controller_name' => 'EleveController',
+            'classes'=> $classes,
         ]);
     }
 }
